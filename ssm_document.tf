14f467c3a52e66918215d5c67a32d6aee9be5d0c
mohammedsaqlain0230 <mohammed@airowire.com>
2026-06-12 00:37:52 +0530

    Update ssm_document.tf

--- ssm_document.tf
+++ ssm_document.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_document" "datadog_agent_install" {
 
-  name          = "Datadog-AmazonLinux-and-Ubuntu-Install-Non-prod"
+  name          = "Datadog-AmazonLinux-and-Ubuntu-Install-Prod"
   document_type = "Command"
 
   content = jsonencode({
